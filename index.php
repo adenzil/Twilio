9e11452a134c2bce6a267556e7dfe9725e76d52f
Bony Carvalho <bony.carvalho082@gmail.com>
2016-10-05 15:03:44 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+ 
+// ==== Control Vars =======
+$strFromNumber = "+16093854774";
+$strToNumber = "+16093699533";
+$strMsg = "Hello from Bony Carvalho";  
+$aryResponse = array();
+ 
+
+    // include the Twilio PHP library - download from 
+    // http://www.twilio.com/docs/libraries/
+    require_once ("inc/Services/Twilio.php");
+ 
+    // set our AccountSid and AuthToken - from www.twilio.com/user/account
+    $AccountSid = "ACb9e36693e13255078834ea97d58c7fa4";
+    $AuthToken = "01a77d3b67d3bdc5810d3988296bdf5d";
+ 
+    // ini a new Twilio Rest Client
+    $objConnection = new Services_Twilio($AccountSid, $AuthToken);
+
+
+    // Send a new outgoinging SMS by POSTing to the SMS resource */
+    $bSuccess = $objConnection->account->sms_messages->create(
+        
+        $strFromNumber, 	// number we are sending From 
+        $strToNumber,           // number we are sending To
+        $strMsg			// the sms body
+    );
+
+		
+    $aryResponse["SentMsg"] = $strMsg;
+    $aryResponse["Success"] = true;
+    
+    
+    echo json_encode($aryResponse);
